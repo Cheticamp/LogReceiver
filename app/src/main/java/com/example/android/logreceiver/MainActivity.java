8c41f4d6959451a5eaa22ccc18df5f4c41dfd133
shopn08@gmail.com <shopn08@gmail.com>
2019-02-26 08:43:26 -0500

    Initial commit

---
+++ app/src/main/java/com/example/android/logreceiver/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.android.logreceiver;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatTextView;
+import android.text.Html;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = getIntent();
+        String intentType = intent.getType();
+        Bundle intentExtras = intent.getExtras();
+
+        if (intentType == null || intentExtras == null) {
+            return;
+        }
+
+        setContentView(R.layout.activity_main);
+        AppCompatTextView textView = findViewById(R.id.textView);
+
+        String text = intent.getExtras().getString(Intent.EXTRA_HTML_TEXT);
+        if (text != null) {
+            textView.setText(Html.fromHtml(text));
+            return;
+        }
+
+        text = intent.getExtras().getString(Intent.EXTRA_TEXT);
+
+        if (text != null) {
+            textView.setText(text);
+        }
+    }
+}
